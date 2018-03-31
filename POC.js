4b207133c2ce3fbc6e626e821ad2bcbff6b43c6e
Saziba <Saziba@air-de-saziba.home>
2018-03-30 22:07:00 -0300

    Implementando content script grandes avancos na automatizacao e no formulario

--- POC.js
+++ POC.js
@@ -1,23 +1,22 @@
 c = $(' #conteudo ')
 ct = (c.contentWindow || c.contentDocument)
-if(ct.document) ct = ct.document
+if (ct.document) ct = ct.document
 ct.querySelectorAll('iframe')
 frms = ct.querySelectorAll('iframe')
-
 acoes = frms[0]
 acoes = acoes.contentWindow || acoes.contentDocument
-if(acoes.document) acoes = acoes.document
-
+if (acoes.document) acoes = acoes.document
 cont = frms[1]
 cont = cont.contentWindow || cont.contentDocument
-if(cont.document) cont = cont.document
-
-
-
+if (cont.document) cont = cont.document
+cont.body.querySelector('#Label19').innerHTML = "Timesheet Hapens &trade; </br> Insira ao lado sua </br> planilha de horas </br> ---->>>>>"
+cont.body.querySelector('#Label19').style = "background-color:#d0ddd0"
+cont.body.querySelector('#txtdsc_atividade').disabled = ""
+cont.body.querySelector('#txtdsc_atividade').placeholder = "Insira aqui sua planilha de horas"
+cont.body.querySelector('#txtdsc_atividade').style = "background-color:#d0ddd0"
 cont.body.querySelector('#txtnum_percentual_concluido').value = "10"
 cont.body.querySelector('#ddlCod_fabr_tipoatividade').value = "1"
 cont.body.querySelector('#txtHorasTrabalhadas').value = "07:30"
 cont.body.querySelector('#txtDataAtividade').value = "24/02/2018"
 cont.body.querySelector('#txtdsc_Observacoes').value = "Atividades referentes ao projeto informado"
-
 acoes.body.querySelector('#botGravar').click()
--- chrome-extension/timesheet_content_script.js
+++ chrome-extension/timesheet_content_script.js
@@ -1,1 +1,68 @@
-document.querySelector('body').setAttribute("bgcolor", "#d0ddd0")
+function acoesOnload() {
+    console.log(this.src);
+    acoesDoc = this.contentWindow || this.contentDocument;
+    if (acoesDoc.document) acoesDoc = acoesDoc.document;
+    /*    
+
+
+    cont.body.querySelector('#txtnum_percentual_concluido').value = "10"
+    cont.body.querySelector('#ddlCod_fabr_tipoatividade').value = "1"
+    cont.body.querySelector('#txtHorasTrabalhadas').value = "07:30"
+    cont.body.querySelector('#txtDataAtividade').value = "24/02/2018"
+    cont.body.querySelector('#txtdsc_Observacoes').value = "Atividades referentes ao projeto informado"
+
+    acoes.body.querySelector('#botGravar').click()    */
+}
+var planilha, planilhaValue;
+
+function gravar() {
+    console.log('gravando...');
+    this.onclick = "";
+    planilhaValue = planilha.value;
+    planilha.value = "";
+    console.log(planilhaValue);
+    this.onclick = gravar;
+}
+
+function conteudoConteudoOnload() {
+    console.log(this.src);
+    if (this.src == "https://parati.trescon.com.br/Sistema3con/fabr_AtividadeDiaria/fabr_AtividadeDiaria_lista.aspx") {
+        contDoc = this.contentWindow || this.contentDocument;
+        if (contDoc.document) contDoc = contDoc.document;
+        planilha = contDoc.body.querySelector('#txtdsc_atividade');
+        label = contDoc.body.querySelector('#Label19');
+        //
+        planilha.disabled = "";
+        planilha.placeholder = "Insira aqui sua planilha de horas no formato HH:MI DD/MM/AAAA. Uma linha por entrada. Ex.: 08:30 22/04/2018";
+        style = planilha.getAttribute('style');
+        planilha.style = style + "background-color:#d0ddd0;";
+        //
+        label.innerHTML = "<strong>Timesheet Happens &trade;</strong></br>Planilha de Horas</br>para gravar clique <strong>aqui</strong>";
+        label.style = "background-color:#d0ddd0";
+        label.onclick = gravar;
+    }
+}
+
+function conteudoOnload() {
+    console.log(this.src);
+    ct = (c.contentWindow || c.contentDocument);
+    if (ct.document) frms = ct.document.querySelectorAll('iframe');
+    console.log(frms);
+    acoes = frms[0];
+    acoes.onload = acoesOnload;
+    cont = frms[1]
+    cont.onload = conteudoConteudoOnload;
+}
+
+function mapeiaFrames() {
+    c = document.querySelector(' #conteudo ')
+    window.open('fabr_AtividadeDiaria/fabr_AtividadeDiaria.aspx', 'conteudo');
+    c.onload = conteudoOnload;
+}
+
+function windowOnload() {
+    console.log('Timesheet Happens is Here!!!');
+    document.querySelector('body').setAttribute("bgcolor", "#d0ddd0");
+    mapeiaFrames();
+}
+window.onload = windowOnload;
